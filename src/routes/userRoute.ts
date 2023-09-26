5b1cd2a0d7b0c256bc8f983d28a9bee69c9c1c1a
Akindele <akindeleapex@gmail.com>
2023-09-26 15:06:38 +0100

    making sense

--- src/routes/userRoute.ts
+++ src/routes/userRoute.ts
@@ -1,20 +1,11 @@
 import express, { Response, Request } from 'express';
-import passport from 'passport';
-require('../utills/google');
 import { userRegistration, getOtp, forgotPassword, resetPassword, verifyOtp, userLogin } from '../controllers/userController';
 import { validateSignupUser, validateLoginUser, validateForgotPassword, validateChangePassword } from '../middlewares/validation';
 
 
 const router = express.Router();
 
-// router.get('/', (req: Request, res: Response) => {
-//     res.send('<a href="http://localhost:8081/"> Login with Google </a>');
-// });
 
-// router.get('/auth/google', (req: Request, res: Response) => {
-//     passport.authenticate('google', { scope: ['profile', 'email'] });
-//     res.send('Welcome to Medly');
-// });
 router.post('/signup', validateSignupUser, userRegistration);
 router.post('/otp', getOtp);
 router.post('/otp/verify', verifyOtp);

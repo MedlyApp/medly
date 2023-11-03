f42d21512338f156c373265830d8efebe0bb757c
Akindele <akindeleapex@gmail.com>
2023-11-03 12:53:52 +0100

    profile

--- src/routes/userRoute.ts
+++ src/routes/userRoute.ts
@@ -4,7 +4,7 @@ import { auth } from "../middlewares/userAuth";
 import {
     userRegistration, getOtp, updateProfile,
     forgotPassword, resetPassword, follow, unfollow, getUserProfile,
-    verifyOtp, updateProfilePicture, userLogin
+    verifyOtp, updateProfilePicture, userLogin, editUserProfile
 } from '../controllers/userController';
 import {
     validateSignupUser, validateLoginUser,
@@ -15,6 +15,8 @@ const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 const router = express.Router();
 
+const editProfile = upload.fields([{ name: 'image', maxCount: 1 }]);
+
 
 // router.get('/auth/google', googleSignIn);
 router.post('/signup', validateSignupUser, userRegistration);
@@ -23,11 +25,12 @@ router.post('/otp/verify', verifyOtp);
 router.post('/login', validateLoginUser, userLogin);
 router.post('/forgot-password', validateForgotPassword, forgotPassword);
 router.post('/reset-password', validateChangePassword, resetPassword);
-router.post('/upload-profile-picture', upload.single('image'), auth, updateProfilePicture);
+// router.post('/upload-profile-picture', upload.single('image'), auth, updateProfilePicture);
 router.patch('/update-profile', auth, updateProfile);
 router.put('/follow', auth, follow);
 router.put('/unfollow', auth, unfollow);
 router.get('/profile', auth, getUserProfile);
+router.put('/edit-profile', editProfile, auth, editUserProfile);
 
 
 export default router;
